8c3077866a1b90a7f096fccb21a3915fae774e83
sawyehtet <sawyehtet845@gmail.com>
2023-05-15 11:46:30 +0630

    array filter and array map

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
-require "./core/boot.php";
-require Router::load("routes.php")->direct(Request::uri());
+// require "./core/boot.php";
+// require Router::load("routes.php")->direct(Request::uri());
+
+
+class Post{
+    public $title;
+    public $isPublish;
+
+    public function __construct($title, $isPublish) 
+    {
+        $this->title = $title;
+        $this->isPublish = $isPublish;
+    }
+
+}
+
+$posts = [
+    new Post("Post 1", true),
+    new Post("Post 2", false),
+    new Post("Post 3", true),
+    new Post("Post 4", false),
+];
+
+// $unPublishPosts = array_filter($posts , function($post) {
+//     return !$post->isPublish;
+// });
+// echo "<pre>";
+// var_dump($unPublishPosts) ;
+
+
+
+$titles = array_map(function($post) {
+    return $post->title;
+},$posts);
+
+echo "<pre>";
+
+var_dump($titles);
